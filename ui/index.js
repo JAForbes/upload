4c86df0438f5d9d3f06a969025187525fb92eb0f
James <james.a.forbes@gmail.com>
2018-01-27 22:58:30 +1100

    Consolidate states into UploadState

--- ui/index.js
+++ ui/index.js
@@ -1,54 +1,109 @@
 const {
     m
-    , FormData
-    , XMLHttpRequest
     , URL
 } = window // eslint-disable-line no-undef
 
-const preview = imageURL => 
-  m('div.bg-black-20.dib.ma0.w-100.h5', {
-    style: {
-      background: imageURL.map(
-        url => `url(${url}) no-repeat center center`  
-      )
-      .pop()
-      ,backgroundSize: 'cover'
-    }
-  })
-  
-const Type = { 
-  cata: cases => o => 
-    cases[o.case]( o.value )
-  
-  ,addCase: (p, k) => {
-    p[k] = value => ({
-        case: k
-        ,value
-    })
-    return p 
-  }
-}
+const dont = i => i
 
-const UploadState = 
-  ['Inactive', 'Signing', 'Uploading', 'Failed']
-    .reduce( Type.addCase, Type )
+const preview = uploadState => 
+    uploadState.case == 'Inactive'
+        ? m('div.bg-black-20.dib.ma0.w-100.h5'
+            ,'Drop Zone'
+        ) 
+        : m('div.bg-black-20.dib.ma0.w-100.h5', {
+            style: {
+                background: 
+                    `url(${uploadState.value.preview}) no-repeat center center`
+                ,backgroundSize: 'cover'
+            }
+        })
+
+const DefineSumType = o => 
+    Object.keys(o)
+        .map( k => [k, o[k]] )
+        .reduce(
+            (p, [ k, ks ]) => {
+
+                const of = value => {
+
+                    const badValue =
+                        ks.length > 0
+                        && (
+                            value == null
+                            || typeof value != 'object'
+                        )
+
+                    if ( badValue ){
+                        throw new TypeError(
+                            k +' expects {'+ks.join(', ')+'} but received: ' 
+                                + value
+                        )
+                    }
+
+                    const missingValues = 
+                        ks.filter(
+                            k => !(k in value)
+                        )
+
+                    if( missingValues.length ){
+                        throw new TypeError(
+                            k + ' is missing expected values: '
+                                + missingValues.join(',')
+                        )
+                    }
+
+                    return { 
+                        case: k
+                        , value
+                    }
+                }
+
+                p[k] = of
+
+                return p
+            }
+            ,{
+                cata: cases => o => 
+                    cases[o.case]( o.value )
+            }
+        )
+
+const UploadState = DefineSumType({
+    Inactive: 
+        []
+    ,Unconfirmed: 
+        ['file', 'id', 'preview']
+    ,Signing: 
+        ['file', 'id', 'preview']
+    ,Uploading: 
+        ['progress', 'id', 'file', 'preview']
+    ,Failed: 
+        ['error', 'id', 'file', 'preview']
+    ,Processing: 
+        ['file', 'id', 'progress', 'preview']
+    ,Processed: 
+        ['file', 'id', 'url', 'preview']
+})
 
 // UploadState -> Boolean
 const buttonDisabled = UploadState.cata({
-  Inactive: () => false
-  ,Uploaded: () => true
+  Inactive: () => true
+  ,Unconfirmed: () => false
+  ,Processing: () => true
+  ,Processed: () => true
   ,Failed: () => false
   ,Uploading: () => true
   ,Signing: () => true
 })
 
-
 // UploadState -> HyperScript
 const uploadStatusMessage = UploadState.cata({
   Inactive: () => 'Waiting for file input.'
-  ,Uploaded: () => 'File uploaded successfully!'
-  ,Failed: error => m('span.red',error.message)
-  ,Uploading: progress => progress+'% Uploaded'
+  ,Unconfirmed: () => 'Upload when ready.'
+  ,Processing: () => 'File is being processed'
+  ,Processed: () => 'File uploaded.  Upload more?'
+  ,Failed: ({error}) => m('span.red',error.message)
+  ,Uploading: ({progress}) => progress+'% Uploaded'
   ,Signing: () => 'Signing ...'
 })
   
@@ -57,98 +112,102 @@ const uploadStatusMessage = UploadState.cata({
 // UploadState -> HyperScript
 const buttonText = UploadState.cata({
   Inactive: () => 'Upload'
-  ,Uploaded: () => 'Upload'
+  ,Unconfirmed: () => 'Upload'
+  ,Processing: () => 'Upload'
+  ,Processed: () => 'Upload'
   ,Failed: () => 'Try Again'
   ,Uploading: () => 'Upload'
   ,Signing: () => 'Upload'
 })
 
-const tap = f => (...xs) => {
-  console.log('tap', xs)
-  return (f(...xs), xs[0])
-}
-
 function App(){
   
-  const imageURL = []
   
-  const uploadState = [
+  let uploadState = 
     UploadState.Inactive()
-  ]
-  
-  const file = []
   
   async function onclick(){
     
     try {
-        uploadState[0] = UploadState.Signing()
+        uploadState = UploadState.Signing(uploadState.value)
 
         const { url } = await m.request(
             { method: 'POST'
             , url: 'https://harth-upload-services.herokuapp.com'
             , data: 
-                { filename: file[0].name
-                , filetype: file[0].type
+                { filename: uploadState.value.file.name
+                , filetype: uploadState.value.file.type
                 }
             }
         )
 
-      const response = await fetch(url, {
-          method: 'PUT'
-          ,body: file
-      })
-      .catch( e => e )
-    //   const response = await m.request({
-    //     method: 'PUT'
-    //     ,url
-    //     ,headers: {
-    //         'Content-Type': 'multipart/form-data'
-    //     }
-    //     ,data: file
-    //   })
-    //   .catch( e => e )
+        await m.request({
+            url
+            ,method: 'PUT'
+            ,data: uploadState.value.file
+            ,headers: 
+                { 'Content-Type': uploadState.value.file.type
+                }
+            ,serialize: dont
+            ,config(xhr){
+                xhr.upload.addEventListener(
+                    'progress'
+                    , uploadProgress
+                    , false
+                )
 
-      console.log(response)
+                xhr.addEventListener(
+                    'load'
+                    , uploadComplete
+                    , false
+                )
 
+                function uploadProgress(e){
 
-    //   function uploadProgress(e){
-    //     uploadState[0] = UploadState.Uploading(
-    //       Math.floor(e.loaded * 100 / e.total)
-    //     )  
-    //     m.redraw()
-    //   }
+                    uploadState = UploadState.Uploading(
+                        Object.assign(
+                            {}, uploadState.value, {
+                                progress: Math.floor(e.loaded * 100 / e.total)
+                            }
+                        )
+                    ) 
+                
+                    m.redraw()
+                }
 
-    //   function uploadComplete(evt) {
-    //     if (evt.target.responseText == "") {
-    //       uploadState[0] = UploadState.Success()
-    //     } else {
-    //       uploadState[0] = UploadState.Failed(
-    //         new Error( 
-    //             evt.target.responseXML
-    //                 .querySelector('Message').innerHTML
-    //         )  
-    //       )
-    //     }
-    //     m.redraw()
-    //   }
+                function uploadComplete(evt) {
+                    if (evt.target.responseText == "") {
+                        uploadState = UploadState.Processing(
+                            Object.assign({
+                                progress: 0
+                            }, uploadState.value)
+                        )
+                    } else {
+                        uploadState = UploadState.Failed(
+                            new Error(
+                                evt.target.responseXML
+                                    .querySelector('Message').innerHTML
+                            )  
+                        )
+                    }
+    
+                    m.redraw()
+                }
 
-    //   var xhr = new XMLHttpRequest();
-    //   xhr.upload.addEventListener("progress", uploadProgress, false);
-    //   xhr.addEventListener("load", uploadComplete, false);
-    //   xhr.open('PUT', signedURL.url);
-    //   xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    //   xhr.send(formData);  
+            }
+        })
     } catch (e) {
-      uploadState[0] = UploadState.Failed( e ) 
+      uploadState = UploadState.Failed( e ) 
       m.redraw()
     }
-    
   }
   
   function onchange(e){
-    imageURL[0] = URL.createObjectURL(e.currentTarget.files[0])
-    file[0] = e.currentTarget.files[0]
-    uploadState[0] = UploadState.Inactive()
+    uploadState = UploadState.Unconfirmed({
+        id: Math.random().toString(15).slice(2)
+        ,file: e.currentTarget.files[0]
+        ,preview: URL.createObjectURL(e.currentTarget.files[0])
+    })
   }
   
   return {
@@ -157,17 +216,18 @@ function App(){
         ,m('input[type=file].pa3.bg-black-20', {
           onchange
         })
-        ,preview (imageURL)
+        , preview( uploadState )
+
         ,m('button.dib.h3.tc', 
           { onclick
-          , disabled: imageURL.length == 0
-            || uploadState.map( buttonDisabled ).pop()
+          , disabled: 
+                buttonDisabled( uploadState )
           }
-          , uploadState.map(buttonText).pop()
+          , buttonText(uploadState)
         )
         ,m('.tc.dib.bg-black-20.black-60.h3.overflow-y-auto.pa1'
             +'.flex.justify-center.items-center'
-           , uploadState.map( uploadStatusMessage ).pop()
+           , uploadStatusMessage(uploadState)
         )
       )
       
